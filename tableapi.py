c55803e1560e43ab5308a4d8273d3ee78054a654
Onni Linnala <onnilinnala@gmail.com>
2023-02-22 12:30:23 +0200

    Lmao Starter

--- tableapi.py
+++ tableapi.py
@@ -11,3 +11,30 @@ def changeKill(row, column, kill):
 
 def getData(row, column, dataType):
     return table[row][column][dataType]
+
+
+def move(piece, target_square):
+    row = piece[0]
+    column = piece[1]
+    target_row = target_square[0]
+    target_column = target_square[1]
+    piece = None
+    is_valid_square = False
+    if table[row][column]["Piece"] is None:
+        pass
+    elif table[row][column]["Piece"] == "1":
+        piece = "Defender"
+    elif table[row][column]["Piece"] == "2":
+        piece = "Attacker"
+    elif table[row][column]["Piece"] == "3":
+        piece = "King"
+    if table[target_row][target_column]["Piece"] is None:
+        is_valid_square = True
+    else:
+        pass
+    if is_valid_square and piece:
+        if column == target_column:
+            if
+        elif row == target_row:
+
+
--- temp3.py
+++ temp3.py
@@ -1,22 +1,24 @@
 import main
+import tableapi
 
 table = main.start()
-print(table)
+
 def printCurrentTable():
-    print("    |1|2|3|4|5|6|7|8|9")
+    print("    |1|2|3|4|5|6|7|8|9|")
     for row in table:
         row_txt = f"({row}) |"
         for column in range(9):
             if table[row][str(column + 1)]["Piece"] is None:
-                row_txt = row_txt + ' |'
+                row_txt = row_txt + f' |'
             elif table[row][str(column + 1)]["Piece"] == "1":
-                row_txt = row_txt + 'D|'
+                row_txt = row_txt + f'D|'
             elif table[row][str(column + 1)]["Piece"] == "2":
-                row_txt = row_txt + 'A|'
+                row_txt = row_txt + f'A|'
             elif table[row][str(column + 1)]["Piece"] == "3":
-                row_txt = row_txt + 'K|'
-            else:
-                row_txt = row_txt + '!|'
+                row_txt = row_txt + f'K|'
         print(row_txt)
 
+move_piece = input("Select piece: ")
+target_square = input("Target square: ")
 printCurrentTable()
+tableapi.move(move_piece, target_square)
