52c592201c57d233c72ebb67a37f624f90aa06c1
ToispuolinenMies <antto.karvonen@outlook.com>
2023-02-22 12:11:41 +0200

    Banana

--- temp3.py
+++ temp3.py
@@ -2,15 +2,21 @@ import main
 
 table = main.start()
 print(table)
-for row in table:
-    row_txt = "|"
-    for column in range(9):
-        if table[row][str(column + 1)]["Piece"] is None:
-            row_txt = row_txt + f' |'
-        elif table[row][str(column + 1)]["Piece"] == "1":
-            row_txt = row_txt + f'D|'
-        elif table[row][str(column + 1)]["Piece"] == "2":
-            row_txt = row_txt + f'A|'
-        elif table[row][str(column + 1)]["Piece"] == "3":
-            row_txt = row_txt + f'K|'
-    print(row_txt)
+def printCurrentTable():
+    print("    |1|2|3|4|5|6|7|8|9")
+    for row in table:
+        row_txt = f"({row}) |"
+        for column in range(9):
+            if table[row][str(column + 1)]["Piece"] is None:
+                row_txt = row_txt + ' |'
+            elif table[row][str(column + 1)]["Piece"] == "1":
+                row_txt = row_txt + 'D|'
+            elif table[row][str(column + 1)]["Piece"] == "2":
+                row_txt = row_txt + 'A|'
+            elif table[row][str(column + 1)]["Piece"] == "3":
+                row_txt = row_txt + 'K|'
+            else:
+                row_txt = row_txt + '!|'
+        print(row_txt)
+
+printCurrentTable()
