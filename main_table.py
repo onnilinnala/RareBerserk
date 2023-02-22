36c069d5711d8b5d6b4de1dd820f954d88bdf4bb
ToispuolinenMies <antto.karvonen@outlook.com>
2023-02-22 10:13:26 +0200

    Banana

---
+++ main_table.py
@@ -0,0 +1,344 @@
+main_table = {
+    "A": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "B": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "C": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "D": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "E": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "F": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "G": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "H": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    },
+    "I": {
+        "1": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "2": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "3": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "4": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "5": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "6": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "7": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "8": {
+            "Piece": None,
+            "KillSquare": False
+        },
+        "9": {
+            "Piece": None,
+            "KillSquare": False
+        },
+    }
+}
