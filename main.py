33d36b06d23e0b07a5dbbb82023376c1b1f0d756
Onni Linnala <onnilinnala@gmail.com>
2023-02-22 16:05:50 +0200

    Lmao Starter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from main_table import main_table as table
+import tableapi
 
 
 def start():
@@ -32,4 +33,48 @@ def start():
     table["I"]["5"]["Piece"] = "1"  # Defender
     table["I"]["6"]["Piece"] = "1"  # Defender
     print(table)
-    return table
+    askForMove()
+
+
+def askForMove():
+    move_piece = input("Select piece: ")
+    # Check validity
+    if len(move_piece) != 2:
+        print("Invalid piece selected!\n")
+        askForMove()
+        return
+    if move_piece[0] == "A" or move_piece[0] == "B" or move_piece[0] == "C" or move_piece[0] == "D" or move_piece[0] == "E" or move_piece[0] == "F" or move_piece[0] == "G" or move_piece[0] == "H" or move_piece[0] == "I":
+        if move_piece[1] == "1" or move_piece[1] == "2" or move_piece[1] == "3" or move_piece[1] == "4" or move_piece[1] == "5" or move_piece[1] == "6" or move_piece[1] == "7" or move_piece[1] == "8" or move_piece[1] == "9":
+            pass
+        else:
+            print("Invalid piece selected!\n")
+            askForMove()
+            return
+    else:
+        print("Invalid piece selected!\n")
+        askForMove()
+
+
+    target_square = input("Target square: ")
+    # Check validity
+    if len(target_square) != 2:
+        print("Invalid piece selected!\n")
+        askForMove()
+        return
+    if target_square[0] == "A" or target_square[0] == "B" or target_square[0] == "C" or target_square[0] == "D" or target_square[0] == "E" or target_square[0] == "F" or target_square[0] == "G" or target_square[0] == "H" or target_square[0] == "I":
+        if target_square[1] == "1" or target_square[1] == "2" or target_square[1] == "3" or target_square[1] == "4" or target_square[1] == "5" or target_square[1] == "6" or target_square[1] == "7" or target_square[1] == "8" or target_square[1] == "9":
+            pass
+        else:
+            print("Invalid piece selected!\n")
+            askForMove()
+            return
+    else:
+        print("Invalid square selected!\n")
+        askForMove()
+        return
+    tableapi.move(move_piece, target_square)
+    askForMove()
+    return
+
+
+start()
--- tableapi.py
+++ tableapi.py
@@ -14,27 +14,154 @@ def getData(row, column, dataType):
 
 
 def move(piece, target_square):
-    row = piece[0]
-    column = piece[1]
-    target_row = target_square[0]
-    target_column = target_square[1]
-    piece = None
-    is_valid_square = False
-    if table[row][column]["Piece"] is None:
-        pass
-    elif table[row][column]["Piece"] == "1":
-        piece = "Defender"
-    elif table[row][column]["Piece"] == "2":
-        piece = "Attacker"
-    elif table[row][column]["Piece"] == "3":
-        piece = "King"
-    if table[target_row][target_column]["Piece"] is None:
-        is_valid_square = True
+    try:
+        row = piece[0]
+        column = piece[1]
+        target_row = target_square[0]
+        target_column = target_square[1]
+        if target_row == row and target_column == column:
+            raise KeyError
+        piece = getData(row, column, "Piece")
+        is_valid_target = False
+        if getData(target_row, target_column, "Piece") is None:
+            is_valid_target = True
+        else:
+            pass
+        if is_valid_target and piece:
+            if column == target_column:
+                if checkColumn(column, row, target_row):
+                    print("Success")
+                    success = True
+                else:
+                    print("CheckColumn Error!\n")
+                    success = False
+            elif row == target_row:
+                if checkRow(row, column, target_column):
+                    print("Success")
+                    success = True
+                else:
+                    print("CheckRow Error!\n")
+                    success = False
+            else:
+                print("Error5")
+                raise KeyError
+        else:
+            print("Error6")
+            raise KeyError
+        if success:
+            changePiece(row, column, None)
+            changePiece(target_row, target_column, piece)
+            printCurrentTable()
+    except KeyError:
+        print("Invalid move!\n")
+
+
+def letterToInt(row):
+    if row == "A":
+        return 1
+    elif row == "B":
+        return 2
+    elif row == "C":
+        return 3
+    elif row == "D":
+        return 4
+    elif row == "E":
+        return 5
+    elif row == "F":
+        return 6
+    elif row == "G":
+        return 7
+    elif row == "H":
+        return 8
+    elif row == "I":
+        return 9
     else:
-        pass
-    if is_valid_square and piece:
-        if column == target_column:
-            if
-        elif row == target_row:
+        return 0
 
 
+def intToLetter(int):
+    if int == 1:
+        return "A"
+    elif int == 2:
+        return "B"
+    elif int == 3:
+        return "C"
+    elif int == 4:
+        return "D"
+    elif int == 5:
+        return "E"
+    elif int == 6:
+        return "F"
+    elif int == 7:
+        return "G"
+    elif int == 8:
+        return "H"
+    elif int == 9:
+        return "I"
+    else:
+        return
+
+
+def printCurrentTable():
+    print("    |1|2|3|4|5|6|7|8|9|")
+    for row in table:
+        row_txt = f"({row}) |"
+        for column in range(9):
+            if table[row][str(column + 1)]["Piece"] is None:
+                row_txt = row_txt + f' |'
+            elif table[row][str(column + 1)]["Piece"] == "1":
+                row_txt = row_txt + f'D|'
+            elif table[row][str(column + 1)]["Piece"] == "2":
+                row_txt = row_txt + f'A|'
+            elif table[row][str(column + 1)]["Piece"] == "3":
+                row_txt = row_txt + f'K|'
+        print(row_txt)
+
+
+def checkRow(row, column, target_column):
+    # Ex. 1 -> 6
+    if int(column) < int(target_column):
+        steps = int(target_column) - int(column)
+        print(f"1: {steps}")
+        for step in range(steps):
+            step = step + 1
+            if getData(row, str(int(column)+step), "Piece") is None:
+                pass
+            else:
+                return False
+        return True
+    # Ex. 6 -> 1
+    elif int(column) > int(target_column):
+        steps = int(column) - int(target_column)
+        print(f"2: {steps}")
+        for step in range(steps):
+            step = step + 1
+            if getData(row, str(int(column)-step), "Piece") is None:
+                pass
+            else:
+                print(row)
+                print(str(int(column)-step))
+                return False
+        return True
+
+
+def checkColumn(column, row, target_row):
+    # Ex. A -> F
+    if letterToInt(row) < letterToInt(target_row):
+        steps = letterToInt(target_row) - letterToInt(row)
+        for step in range(steps):
+            step = step + 1
+            if table[intToLetter(letterToInt(row) + step)][column]["Piece"] is None:
+                pass
+            else:
+                return False
+        return True
+    elif letterToInt(row) > letterToInt(target_row):
+        steps = letterToInt(row) - letterToInt(target_row)
+        for step in range(steps):
+            step = step + 1
+            if table[intToLetter(letterToInt(row) - step)][column]["Piece"] is None:
+                pass
+            else:
+                return False
+        return True
